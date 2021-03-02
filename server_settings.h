b9ad14ff67e99d33f919e1db414ff40bc80dff90
Russian Spy <54362776+FueledAmp@users.noreply.github.com>
2021-03-02 12:57:33 +0600

    Update server_settings.h

--- server_settings.h
+++ server_settings.h
@@ -1,5 +1,4 @@
 
-/******************************************************************************/
 #define DEFAULT_PORT 10000 // The port will fall back to this unless told otherwise.
 #define LOG_FILE "chat.log"
 #define MAX_CLIENTS	25
